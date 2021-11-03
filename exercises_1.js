0d7dbcdab8105afe06bf2d557e29f622f47b9c00
Alex Waller <alex@artofalexwaller.com>
2021-11-03 01:17:34 -0400

    FizzBuzz

--- exercises_1.js
+++ exercises_1.js
@@ -491,3 +491,41 @@ console.log(voteCount(votes2));
 
 console.log("-------------------------------------------");
 console.log("-----------------------------------------");
+
+// fizzBuzz function
+
+function fizzBuzz(num = 15) {
+  let i = 1;
+
+  for (; i <= num; i++) {
+    if (i % 5 == 0 && i % 3 == 0) {
+      console.log("FizzBuzz");
+    } else if (i % 5 == 0) {
+      console.log("Fizz");
+    } else if (i % 3 == 0) {
+      console.log("Buzz");
+    } else console.log(i);
+  }
+}
+
+fizzBuzz();
+
+/* 
+
+1
+2
+Buzz
+4
+Fizz
+Buzz
+7
+8
+Buzz
+Fizz
+11
+Buzz
+13
+14
+FizzBuzz
+
+*/
