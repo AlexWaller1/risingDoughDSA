38fb1a81bfc1bc8ea9a3ec217713c5496b023839
Alex Waller <alex@artofalexwaller.com>
2021-11-24 12:15:21 -0500

    trueDuplicates()

--- exercises_Nov24.js
+++ exercises_Nov24.js
@@ -104,3 +104,52 @@ appearsMost(nums3);
 
 console.log("--------------------------------------------");
 console.log("-------------------------------------------");
+
+/* 
+
+Given an array of integers, find if the array contains any
+duplicates.
+
+Your function should return true if any value appears at
+least twice in the array, and it should return false if
+every element is distinct.
+
+Example 1:
+
+Input: [1, 2, 3, 1] Output: true
+
+Example 2:
+
+Input: [1, 2, 3, 4] Output: false
+
+*/
+
+let duplicates1 = [1, 2, 3, 1];
+
+let duplicates2 = [1, 2, 3, 4];
+
+let duplicates3 = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
+
+function trueDuplicates(array) {
+  array = sortNum(array);
+  let rBoolean = "";
+  let i = 0;
+
+  for (; i < array.length; i++) {
+    if (array[i] == array[i + 1]) {
+      rBoolean = true;
+      break;
+    } else rBoolean = false;
+  }
+  console.log(rBoolean);
+}
+
+trueDuplicates(duplicates1);
+// true
+trueDuplicates(duplicates2);
+// false
+trueDuplicates(duplicates3);
+// true
+
+console.log("--------------------------------------------");
+console.log("-------------------------------------------");
