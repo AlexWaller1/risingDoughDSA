5bb505cfa7348f5dbfecf633434c90afa156e543
Alex Waller <alex@artofalexwaller.com>
2021-11-24 12:30:13 -0500

    targetSum()

--- exercises_Nov24.js
+++ exercises_Nov24.js
@@ -153,3 +153,65 @@ trueDuplicates(duplicates3);
 
 console.log("--------------------------------------------");
 console.log("-------------------------------------------");
+
+/* 
+
+Given an array of integers nums and an integer target, 
+return indices of the two numbers such that they add up to
+the target.
+
+You may assume that each input would have exactly one 
+solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1: 
+
+Input: nums = [2, 7, 11, 15], target = 9
+
+Output: [0, 1];
+
+Example 2:
+
+Input: nums = [3, 2, 4], target = 6
+
+Output: [1, 2]
+
+Input: nums = [3, 2, 4], target = 6
+
+Output: [0, 1]
+
+*/
+
+let nums10 = [2, 7, 11, 15];
+
+let nums11 = [3, 2, 4];
+
+let nums12 = [3, 3];
+
+function targetSum(array, target) {
+  let indices = [];
+  let i = 0;
+
+  for (; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] == target) {
+        indices = [i, j];
+        break;
+      }
+    }
+  }
+  console.log(indices);
+}
+
+targetSum(nums10, 9);
+// [0, 1]
+targetSum(nums11, 6);
+// [1, 2]
+targetSum(nums12, 6);
+// [0, 1]
+targetSum(nums10, 100);
+// []
+
+console.log("---------------------------------------------");
+console.log("--------------------------------------------");
